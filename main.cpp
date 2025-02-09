f133c9555c8827d78af7855ce6373de109e9f925
Simpson Eng <simpsoneng552@gmail.com>
2025-02-09 23:12:43 +0100

    User raylib

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include <string>
-#include <SDL.h>
 
+#include "raylib.h"
 #include "chip8.h"
 #include "config.h"
 
@@ -9,13 +9,17 @@ int main(int argc, char* argv[]) {
     Chip8 emulator;
     const std::string filename = argv[1];
 
-    SDL_Init(SDL_INIT_VIDEO);
+    InitWindow(kDisplayWidth, kDisplayHeight, "Chip 8 Emulator");
 
-    SDL_Window* window = SDL_CreateWindow("Chip 8 Emulator",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, kDisplayWidth, kDisplayHeight, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
-    while(true) {
-
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
     }
+
+    CloseWindow();
+
+    return 0;
 }
